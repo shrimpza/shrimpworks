f6ea3bbb69d9b25e227cf7ef080c0fc88fd46f4b
Kenneth Watson <git@shrimpworks.za.net>
2016-03-29 16:54:58 +0200

    add comments

--- script/lightbox.js
+++ script/lightbox.js
@@ -1,13 +1,16 @@
-
 (function() {
 
-  var lightbox;
-  var img;
+  var lightbox; // lightbox parent element
+  var img;      // image element
 
   document.addEventListener('DOMContentLoaded', function () {
     this.initialise();
   }.bind(this));
 
+  /**
+   * Create the lightbox and image elements, and add them to the body
+   * of the document.
+   */
   this.initialise = function() {
     this.lightbox = document.createElement('a');
     this.img = document.createElement('img');
@@ -19,12 +22,22 @@
 
     document.getElementsByTagName('body')[0].appendChild(this.lightbox);
 
+    /* now we can look for image links on the page */
     this.scanImages();
   }
 
+  /**
+   * Search through all links to images on the page. Any image link will
+   * have it's target appear in the lightbox, if Javascript is available,
+   * otherwise it will function as a normal link, still supporting middle-
+   * clicking et al.
+   */
   this.scanImages = function() {
+    /* find links to png, jpg and gif images */
     var links = document.querySelectorAll('a[href$=".png"], a[href$=".jpg"], a[href$=".gif"]');
 
+    /* for each link attach a click handler to set the image location and
+       show the lightbox */
     Array.prototype.forEach.call(links, function(l) {
       l.addEventListener('click', function(e) {
         e.preventDefault();
@@ -35,6 +48,9 @@
     }.bind(this));
   }
 
+  /**
+   * Set the image element source to the provided URL.
+   */
   this.setImage = function(img) {
     this.img.setAttribute('src', img);
   }
